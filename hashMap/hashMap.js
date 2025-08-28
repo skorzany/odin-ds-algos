fe8702e1e7c5421bf7615d0b31c93ec8f6f6013c
skorzany <177423232+skorzany@users.noreply.github.com>
2025-08-28 16:46:39 +0200

    Refactor and cleanup the class, update the tests

--- hashMap/hashMap.js
+++ hashMap/hashMap.js
@@ -1,184 +1,167 @@
+/* eslint-disable no-underscore-dangle */
 class Node {
   constructor(key, value) {
     this.key = key;
-    this.value = value;
-    this.nextNode = null;
+    this.data = value;
+    this.next = null;
   }
 }
 
 export default class HashMap {
   constructor() {
     this.LOAD_FACTOR = 0.75;
-    this.BASE_CAPACITY = 16;
-    this.capacity = this.BASE_CAPACITY;
+    this.CAPACITY_BASE = 16;
+    this.buckets = new Array(this.CAPACITY_BASE);
+    this.capacityCurrent = this.CAPACITY_BASE;
     this.entryCount = 0;
-    this.buckets = new Array(this.BASE_CAPACITY);
   }
 
-  // create and return a hash code for given key
+  // create and return the hash code for the given key (will throw an error if the key is not a string, or if the produced hashCode falls out of the current bounds)
   hash(key) {
+    if (typeof key !== 'string') throw new Error('Invalid key type');
     let hashCode = 0;
     const primeNumber = 31;
     for (let i = 0; i < key.length; i += 1) {
       hashCode = primeNumber * hashCode + key.charCodeAt(i);
       // long keys can produce a very large number which exceeds JS's integer limit,
       // so to keep the precision we do modulo division on each iteration instead once at the end:
-      hashCode %= this.capacity;
+      hashCode %= this.capacityCurrent;
     }
+    if (hashCode < 0 || this.capacityCurrent <= hashCode)
+      throw new Error('Trying to access index out of bounds');
     return hashCode;
   }
 
-  // set a key-value pair in the map
+  // set the new key-value pair in the map, or update the existing one
   set(key, value) {
     const index = this.hash(key);
-    if (index < 0 || this.capacity <= index)
-      throw new Error('Trying to access index out of bounds');
-    let nodeLast = this.buckets[index];
-    if (!nodeLast) {
-      this.buckets[index] = new Node(key, value);
-    } else {
-      while (true) {
-        if (nodeLast.key === key) {
-          nodeLast.value = value;
-          return;
-        } else if (nodeLast.nextNode === null) {
-          nodeLast.nextNode = new Node(key, value);
-          break;
-        } else nodeLast = nodeLast.nextNode;
-      }
-    }
-    this.entryCount += 1;
-
-    const isOverloaded = this.LOAD_FACTOR * this.capacity < this.entryCount;
-    if (isOverloaded) {
-      this.capacity *= 2;
-      this._rebuild();
-    }
-  }
-
-  // return value assigned to a key or null if the key is absent
-  get(key) {
-    const index = this.hash(key);
-    if (index < 0 || this.capacity <= index)
-      throw new Error('Trying to access index out of bounds');
+    let nodeParent = null;
     let nodeCurrent = this.buckets[index];
     while (nodeCurrent) {
-      if (nodeCurrent.key === key) return nodeCurrent.value;
-      nodeCurrent = nodeCurrent.nextNode;
+      if (nodeCurrent.key === key) {
+        nodeCurrent.data = value;
+        return;
+      }
+      [nodeParent, nodeCurrent] = [nodeCurrent, nodeCurrent.next];
+    }
+    if (nodeParent === null) this.buckets[index] = new Node(key, value);
+    else nodeParent.next = new Node(key, value);
+    this.entryCount += 1;
+
+    const isOverloaded =
+      this.LOAD_FACTOR * this.capacityCurrent < this.entryCount;
+    if (isOverloaded) this._rebuild();
+  }
+
+  // return the value assigned to the given key, or null if the key is absent
+  get(key) {
+    const index = this.hash(key);
+    let nodeCurrent = this.buckets[index];
+    while (nodeCurrent) {
+      if (nodeCurrent.key === key) return nodeCurrent.data;
+      nodeCurrent = nodeCurrent.next;
     }
     return null;
   }
 
-  // check if the map contains specific key
+  // check if the map contains the given key
   has(key) {
     const index = this.hash(key);
-    if (index < 0 || this.capacity <= index)
-      throw new Error('Trying to access index out of bounds');
     let nodeCurrent = this.buckets[index];
     while (nodeCurrent) {
       if (nodeCurrent.key === key) return true;
-      nodeCurrent = nodeCurrent.nextNode;
+      nodeCurrent = nodeCurrent.next;
     }
     return false;
   }
 
-  // remove the node with given key fom the map & return true; false otherwise
+  // remove the node with the given key fom the map & return true; false otherwise
   remove(key) {
     const index = this.hash(key);
-    if (index < 0 || this.capacity <= index)
-      throw new Error('Trying to access index out of bounds');
-    let nodeBefore = this.buckets[index];
-    if (!nodeBefore) return false;
-    else if (nodeBefore.key === key) {
-      this.buckets[index] = nodeBefore.nextNode ?? undefined;
+    let nodeParent = null;
+    let nodeCurrent = this.buckets[index];
+    while (nodeCurrent) {
+      if (nodeCurrent.key === key) break;
+      [nodeParent, nodeCurrent] = [nodeCurrent, nodeCurrent.next];
+    }
+    if (nodeCurrent) {
+      if (nodeParent === null) this.buckets[index] = undefined;
+      else nodeParent.next = nodeCurrent.next;
       this.entryCount -= 1;
       return true;
-    } else {
-      let nodeCurrent = nodeBefore.nextNode;
-      while (nodeCurrent) {
-        if (nodeCurrent.key === key) {
-          nodeBefore.nextNode = nodeCurrent.nextNode;
-          this.entryCount -= 1;
-          return true;
-        }
-        nodeBefore = nodeCurrent;
-        nodeCurrent = nodeBefore.nextNode;
-      }
-      return false;
     }
+    return false;
   }
 
-  // number of keys stored in the map
+  // return the number of key-value pairs stored in the map
   length() {
     return this.entryCount;
   }
 
   // remove all entries from the map and restore the defaults
   clear() {
-    this.capacity = this.BASE_CAPACITY;
+    this.buckets = new Array(this.CAPACITY_BASE);
+    this.capacityCurrent = this.CAPACITY_BASE;
     this.entryCount = 0;
-    this.buckets = new Array(this.BASE_CAPACITY);
   }
 
-  // return array with all keys from the map
+  // return the array containing all of the keys from the map
   keys() {
     const keysSeen = [];
-    for (let i = 0; i < this.capacity; i += 1) {
-      let nodeCurrent = this.buckets[i];
+    this.buckets.forEach((bucket) => {
+      let nodeCurrent = bucket;
       while (nodeCurrent) {
         keysSeen.push(nodeCurrent.key);
-        nodeCurrent = nodeCurrent.nextNode;
+        nodeCurrent = nodeCurrent.next;
       }
-    }
+    });
     return keysSeen;
   }
 
-  // return array with all values from the map
+  // return the array containing all of the values from the map
   values() {
     const valuesSeen = [];
-    for (let i = 0; i < this.capacity; i += 1) {
-      let nodeCurrent = this.buckets[i];
+    this.buckets.forEach((bucket) => {
+      let nodeCurrent = bucket;
       while (nodeCurrent) {
-        valuesSeen.push(nodeCurrent.value);
-        nodeCurrent = nodeCurrent.nextNode;
+        valuesSeen.push(nodeCurrent.data);
+        nodeCurrent = nodeCurrent.next;
       }
-    }
+    });
     return valuesSeen;
   }
 
-  // return array with each key-value pair from the map
+  // return the array containing all of the key-value pairs from the map
   entries() {
     const entriesSeen = [];
-    for (let i = 0; i < this.capacity; i += 1) {
-      let nodeCurrent = this.buckets[i];
+    this.buckets.forEach((bucket) => {
+      let nodeCurrent = bucket;
       while (nodeCurrent) {
-        const entry = [nodeCurrent.key, nodeCurrent.value];
-        entriesSeen.push(entry);
-        nodeCurrent = nodeCurrent.nextNode;
+        entriesSeen.push([nodeCurrent.key, nodeCurrent.data]);
+        nodeCurrent = nodeCurrent.next;
       }
-    }
+    });
     return entriesSeen;
   }
 
-  // repopulate the enlarged bucket array
+  // double the current capacity and recreate the new buckets array from the old one
   _rebuild() {
-    const newBuckets = new Array(this.capacity);
-    for (let i = 0; i < this.buckets.length; i += 1) {
-      let nodeOld = this.buckets[i];
+    this.capacityCurrent *= 2;
+    const newBuckets = new Array(this.capacityCurrent);
+    this.buckets.forEach((bucket) => {
+      let nodeOld = bucket;
       while (nodeOld) {
         const newIndex = this.hash(nodeOld.key);
-        if (newIndex < 0 || this.capacity <= newIndex)
-          throw new Error('Trying to access index out of bounds');
-        const nodeToAdd = new Node(nodeOld.key, nodeOld.value);
-        let nodeNewLast = newBuckets[newIndex];
-        if (!nodeNewLast) newBuckets[newIndex] = nodeToAdd;
-        else {
-          while (nodeNewLast.nextNode) nodeNewLast = nodeNewLast.nextNode;
-          nodeNewLast.nextNode = nodeToAdd;
-        }
-        nodeOld = nodeOld.nextNode;
+        let nodeParent = null;
+        let nodeCurrent = newBuckets[newIndex];
+        while (nodeCurrent)
+          [nodeParent, nodeCurrent] = [nodeCurrent, nodeCurrent.next];
+        if (nodeParent === null)
+          newBuckets[newIndex] = new Node(nodeOld.key, nodeOld.data);
+        else nodeParent.next = new Node(nodeOld.key, nodeOld.data);
+        nodeOld = nodeOld.next;
       }
-    }
+    });
     this.buckets = newBuckets;
   }
 }
--- hashMap/main.js
+++ hashMap/main.js
@@ -1,9 +1,19 @@
 import HashMap from './hashMap.js';
 
-// instantiation and setting values
 const test = new HashMap();
+// hashing:
 console.log(test.hash('grape') === test.hash('hat')); // true
 console.log(test.hash('apple') !== test.hash('grape')); // true
+try {
+  test.hash([1, 2, 3]);
+} catch {
+  console.log('***hashing non-string key handled***');
+}
+// empty map operations:
+console.log(test.entryCount === 0); // true
+console.log(test.remove('abcd') === false); // true
+console.log(test.entryCount !== -1); // true, counter should not decrease
+// adding stuff, but not exceeding the load factor:
 test.set('apple', 'red');
 test.set('banana', 'yellow');
 test.set('carrot', 'orange');
@@ -16,39 +26,43 @@ test.set('ice cream', 'white');
 test.set('jacket', 'blue');
 test.set('kite', 'pink');
 test.set('lion', 'golden');
-console.log(test.length()); // 12
-console.log(test.capacity); // 16, as the load factor was not exceeded
+console.log(test.length() === 12); // true
+console.log(test.capacityCurrent === 16); // true, as the load factor was not exceeded
+// updating existing key-value pairs:
 test.set('hat', 'fancy-new-color');
 test.set('banana', 'brown-yellow');
-console.log(test.length()); // still 12
-console.log(test.capacity); // still 16
+console.log(test.length() === 12); // true
+console.log(test.capacityCurrent === 16); // true, we only updated some existing keys
+// adding new pair, which should exceed the limit and trigger the rebuild:
 test.set('moon', 'silver');
-console.log(test.length()); // 13
-console.log(test.capacity); // 32, as we added new node which broke the load limit
+console.log(test.length() === 13); // true, added a *new* key-value pair
+console.log(test.capacityCurrent === 32); // true, adding a *new* pair exceeded the load factor
+// updating keys in the enlarged bucket structure:
 test.set('apple', 'green');
 test.set('dog', 'black');
-console.log(test.length()); // still 13
-console.log(test.capacity); // still 32
-// other methods
-console.log(test.get('elephant')); // 'gray'
-console.log(test.get('coin')); // null
-console.log(test.has('carrot')); // true
-console.log(test.has('trash')); // false
+console.log(test.length() === 13); // true,
+console.log(test.capacityCurrent === 32); // true, we only updated some existing keys
+// lookup methods & removal:
+console.log(test.get('elephant') === 'gray'); // true
+console.log(test.get('coin') === null); // true
+console.log(test.has('carrot') === true); // true
+console.log(test.has('trash') === false); // true
 console.log(test.keys()); // it might be not in order, but roughly: ['apple', 'banana', 'carrot', 'dog', 'elephant', 'frog', 'grape', 'hat', 'ice cream', 'jacket', 'kite', 'lion', 'moon']
 console.log(test.values()); // it might be not in order, but roughly: ['green', 'brown-yellow', 'orange', 'black', 'gray', 'green', 'purple', 'fancy-new-color', 'white', 'blue', 'pink', 'golden', 'silver']
 console.log(test.entries()); // it might be not in order, but roughly: [['apple', 'green'], ['banana', 'brown-yellow'], ['carrot', 'orange'], ['dog', 'black'], ['elephant', 'gray'], ['frog', 'green'], ['grape', 'purple'], ['hat', 'fancy-new-color'], ['ice cream', 'white'], ['jacket', 'blue'], ['kite', 'pink'], ['lion', 'gold'], ['moon', 'silver']]
-console.log(test.remove('nonexistentKey')); // false
-console.log(test.length()); // still 13
-console.log(test.capacity); // still 32
-console.log(test.remove('hat')); // true
-console.log(test.length()); // 12
-console.log(test.capacity); // still 32 as there's no mechanism to reduce the capacity other than clearing everything
-console.log(test.entries()); // it might be not in order, but roughly: [['apple', 'green'], ['banana', 'brown-yellow'], ['carrot', 'orange'], ['dog', 'black'], ['elephant', 'gray'], ['frog', 'green'], ['grape', 'purple'], ['ice cream', 'white'], ['jacket', 'blue'], ['kite', 'pink'], ['lion', 'gold'], ['moon', 'silver']]
-console.log(test.remove('apple')); // true (test for removing from the start as 'apple' was added first)
-console.log(test.length()); // 11
+console.log(test.remove('nonexistentKey') === false); // true
+console.log(test.length() === 13); // true, looking for stuff, or removing nonexistent keys do not change the state of the map
+console.log(test.capacityCurrent === 32); // true, same reason as above
+console.log(test.remove('hat') === true); // true
+console.log(test.length() === 12); // true, now we have removed an existing key, so the counter decreases
+console.log(test.capacityCurrent === 32); // still 32 as there's no mechanism to reduce the capacity other than clearing everything
+console.log(test.entries()); // same output as before, minus ['hat', 'fancy-new-color']
+console.log(test.remove('apple') === true); // true
+console.log(test.length() === 11); // true
+// resetting the map
 test.clear();
-console.log(test.length()); // 0
-console.log(test.capacity); // 16, back to the default
-console.log(test.entries()); // []
-console.log(test.values()); // []
-console.log(test.keys().length); // 0
+console.log(test.length() === 0); // true
+console.log(test.capacityCurrent === 16); // true, back to the default
+console.log(test.entries().length === 0); // true
+console.log(test.values().length === 0); // true
+console.log(test.keys()); // []
